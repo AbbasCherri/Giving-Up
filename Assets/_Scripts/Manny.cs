c7a775303315f9ee1dc476652f2305a457904e9c
Hadi Hussein <hadihussein1234.hh@gmail.com>
2026-04-27 21:47:52 +0300

    Manny Walk and Attack

--- Assets/_Scripts/Manny.cs
+++ Assets/_Scripts/Manny.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
+using Vector3 = UnityEngine.Vector3;
 
 public class Manny_Move : MonoBehaviour
 {
     [SerializeField] public NavMeshAgent agent;
     public Transform player;
+    public Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     void Update()
     {
-        if (player)
+        animator.SetFloat("speed",agent.velocity.magnitude);
+        if (Vector3.Distance(transform.position, player.position) < 10f)
         {
             agent.SetDestination(player.position);
+            if ( Vector3.Distance(transform.position,player.position)>5f)
+            {
+                agent.speed = 3.5f;
+            }
+            else if (Vector3.Distance(transform.position, player.position) < 5f)
+            {
+                agent.speed = 6f;
+            }
         }
+        if(CompareTag("Player"))
+
     }
 }
